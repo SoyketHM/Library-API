2419e62ae05342a37a55628e3503ca9ddbffd4c9
SoyketHM <2020belayethossain@gmail.com>
2020-12-08 16:46:31 +0600

    initial server setup

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express      = require('express');
+const app          = express();
+// const db           = require('./db/db');
+// const routes       = require('./routes');
+// const auth         = require('./middlewares/auth');
+// const errorHandler = require('./middlewares/errors');
+const cors		   = require('cors');
+
+require('dotenv').config();
+
+app.use(express.json());
+app.use(cors());
+// app.use('/api', auth.jwt);
+// app.use(routes);
+// app.use(errorHandler);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`server listening on ${PORT}...`);
+});
+
+module.exports = app;
