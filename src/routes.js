0f8a2fc23b2d36e1fa5bfa3c5db4b51db1ef58a8
SoyketHM <2020belayethossain@gmail.com>
2020-12-09 15:45:25 +0600

    done acl

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const router				    =	require('express').Router();
 const health				    =	require('./controllers/health');
 const userController	        =	require('./controllers/user');
 const bookController	        =	require('./controllers/book');
-const regBookController	=	require('./controllers/registerBook');
+const regBookController	        =	require('./controllers/registerBook');
 
 const userValidator		        =	require('./middlewares/userValidator');
 const bookValidator		        =	require('./middlewares/bookValidator');
@@ -14,8 +14,8 @@ router.get('/', health.loopback);
 router.get('/health', health.check);
 
 // User Routes
-router.post('/signup', userValidator.userValidator, checkInvalid, userController.createUser);
 router.post('/login', userValidator.loginValidator, checkInvalid, userController.loginUser);
+router.post('/api/signup', userValidator.userValidator, checkInvalid, userController.createUser);
 router.get('/api/user', userController.getUsers);
 router.get('/api/user/:id', userController.getUserById);
 router.put('/api/user/:id', userValidator.userValidator, checkInvalid, userController.updateUserById);
@@ -27,7 +27,7 @@ router.get('/api/book/:id', bookController.getBookById);
 router.put('/api/book/:id', bookValidator.bookValidator, checkInvalid, bookController.updateBookById);
 router.delete('/api/book/:id', bookController.deleteBookById);
 
-// Book Routes
+// Register Book Routes
 router.post('/api/register-book', registerBookValidator.registerBookValidator, checkInvalid, regBookController.createRegister);
 router.get('/api/register-book', regBookController.getRegisters);
 router.get('/api/register-book/:id', regBookController.getRegisterById);
